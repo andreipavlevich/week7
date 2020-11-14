a01bd0d89be3c71abf8ea0441210464638370e55
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-11-14 22:22:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 require ('http')
 .Server((req, res) => {
-  if (req.url === '/result4/') {
-    const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
-};
-const result = {
-  message:'andreipavlevich',
-  'x-result':req.headers['x-test'],
-};
-  let body = '';
-  req
-  .on('data', b => (body += b))
-  .on('end', () => {
-    result['x-body'] = body;
-    res.writeHead(200, { ...CORS,'Content-Type': 'application/json',});
-    res.end(JSON.stringify(result));
-  });
-  }
+res.end('OK!!!')
 })
 .listen(4321);
