c909c434e2a5fecf1eedbc827eb15bf58b63540f
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-11-14 23:00:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require ('http')
-.Server((req, res) => {
-res.end('OK!!!')
-})
-.listen(process.env.PORT);
+var http = require ('http')
+http.createServer((req, res) {
+  res.writeHead(200, {"Content-Type":"text/plain"})
+  res.end("OK!!!\n")
+}).listen(process.env.PORT)
