488a64e15392dd6c65e407ab8db2a914747aeaf0
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-11-27 00:08:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-export default function hello()
-{
-    console.log("Hello World");
-}
+export default (express, bodyParser, createReadStream, crypto, http) => {
+    const app = express();
+
+
+    const CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers':'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+        }; 
+    
+
+    app
+    .use((r, res, next) => { r.res.set(CORS); next(); })
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get('/sha1/:input', r => {
+    
+    })
+    
+    .get('/login/', (req, res) => res.send('goss'))
+    .get('/code/', (req, res) => {
+ 
+    })
+    ;
+
+    app.all('/req/', (req, res) => {
+         
+    })
+ 
+     
+    return app;
+
+};
